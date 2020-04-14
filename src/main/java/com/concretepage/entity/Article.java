6d594057053ea33fd02c24d8dc9557b9d11de5ec
FerBarragan10 <fer7_10@hotmail.com>
2020-04-14 12:04:05 -0300

    cambio de article_id sino no me mostraba en soap

--- src/main/java/com/concretepage/entity/Article.java
+++ src/main/java/com/concretepage/entity/Article.java
@@ -15,7 +15,7 @@ public class Article implements Serializable {
 	private static final long serialVersionUID = 1L;
 	@Id
 	@GeneratedValue(strategy=GenerationType.AUTO)
-	@Column(name="articleId")
+	@Column(name="article_id")
         private long articleId;  
 	@Column(name="title")
         private String title;
